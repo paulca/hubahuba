fbca6041034392d2e4095fb8cfbdd2720455827f
Paul Campbell <paul@rslw.com>
2009-03-02 13:19:34 +0000

    add string split

--- lib/string.rb
+++ lib/string.rb
@@ -6,4 +6,12 @@ class String
   def to_sha1
     Digest::SHA1.hexdigest(self)
   end
+  
+  def strip(char)
+     new_str = ""
+     self.each_byte do |byte|
+        new_str << byte.chr unless byte.chr == char
+     end
+     new_str
+  end
 end
