2c41a21136371fb8717170a84933112a55ec1f7a
Paul Campbell <paul@rslw.com>
2009-03-02 13:31:27 +0000

    use gsub

--- lib/string.rb
+++ lib/string.rb
@@ -7,25 +7,8 @@ class String
     Digest::SHA1.hexdigest(self)
   end
   
-  alias_method :strip_old, :strip
-  def strip(char=" ")
-    return strip_old if char == nil
-    new_str = ""
-    buff = ""
-    state = :start
-    self.each_byte do |byte|
-      state = :middle if state == :start and byte.chr != char
-
-      if state == :middle and byte.chr == char
-        buff < 0
-        new_str += buff
-        buff = ""
-      end
-
-      new_str << byte.chr
-    end
-
-    new_str
+  def strip_chars(chars)
+    self.gsub(/^[#{chars}]+|[#{chars}]+$/, '')
   end
 end
 
